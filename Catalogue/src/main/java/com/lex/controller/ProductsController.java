26579f968e92564382707b6db390cea3e57916e7
Lex Olov <lekssolov@gmail.com>
2024-12-20 18:05:11 +0600

    Added image loading, changed css for catalog

--- Catalogue/src/main/java/com/lex/controller/ProductsController.java
+++ Catalogue/src/main/java/com/lex/controller/ProductsController.java
@@ -3,11 +3,18 @@ package com.lex.controller;
 import com.lex.payload.NewProductPayload;
 import com.lex.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductsController {
 
     private final ProductService productService;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String catalogue() {
@@ -33,9 +43,26 @@ public class ProductsController {
     }
 
     @PostMapping("manager/create")
-    public String createProduct(Model model, NewProductPayload payload) {
+    public String createProduct(Model model, NewProductPayload payload,
+                                @RequestParam("file")MultipartFile file
+    ) throws IOException {
         model.addAttribute("payload", payload);
-        this.productService.createProduct(payload.title(), payload.info(), payload.img(), payload.price());
+
+        if (!file.isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs();
+            }
+
+            String uuidFile=UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            this.productService.createProduct(payload.title(), payload.info(), resultFilename, payload.price());
+        }
+
         return "redirect:/Store/catalogue";
     }
 }
--- Catalogue/src/main/java/com/lex/payload/NewProductPayload.java
+++ Catalogue/src/main/java/com/lex/payload/NewProductPayload.java
@@ -4,6 +4,5 @@ package com.lex.payload;
 public record NewProductPayload(
         String title,
         String info,
-        String img,
         int price) {
 }
